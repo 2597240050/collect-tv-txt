61727965e285003e3145755c208f3dee6f22890e
2597240050 <52181606+2597240050@users.noreply.github.com>
2024-08-09 14:53:26 +0800

    Update blacklist.py

--- blacklist/blacklist.py
+++ blacklist/blacklist.py
@@ -168,30 +168,7 @@ def clean_url(lines):
 if __name__ == "__main__":
     # 定义要访问的多个URL
     urls = [
-        'https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u',
-        'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u',
-        'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt',
-        'https://raw.githubusercontent.com/kimwang1978/tvbox/main/%E5%A4%A9%E5%A4%A9%E5%BC%80%E5%BF%83/lives/%E2%91%AD%E5%BC%80%E5%BF%83%E7%BA%BF%E8%B7%AF.txt',
-        'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt', 
-        'https://raw.githubusercontent.com/gdstchdr1/IPTV/main/bc.txt',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/skddyj/iptv/main/IPTV.m3u',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/wwb521/live/main/tv.m3u',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/lalifeier/IPTV/main/txt/IPTV.txt',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/yoursmile66/TVBox/main/live.txt',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/hujingguang/ChinaIPTV/main/cnTV_AutoUpdate.m3u8', #15分钟更新1次   #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt', #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt', #ADD 【2024-08-02 16:48:40】#每日更新1次
-        'https://notabug.org/vnjd/yydu/raw/master/yyfug.txt', #ADD 【2024-08-06】
-        'https://tvkj.top/tvlive.txt', #ADD 【2024-08-06】
-        'https://pan.beecld.com/f/OXMcA/%E6%98%A5%E8%B5%A2%E5%A4%A9%E4%B8%8B.txt', #ADD 【2024-08-06】
-        'http://kxrj.site:55/lib/kx2024.txt',   #ADD 【2024-08-07】
-        'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.txt',   #ADD 2024-08-05 每天更新一次
-        'https://raw.githubusercontent.com/balala2oo8/iptv/main/o.m3u',   #ADD 【2024-08-07】#每日更新2次
-        'https://wzsvip.github.io/ipv4.txt',   #ADD 【2024-08-08】
-        'http://wz.42web.io/ipv4.txt',   #ADD 【2024-08-08】
-        'https://wzsvip.github.io/ipv4.m3u'   #ADD 【2024-08-08】
-        #'',
-        #''
+     'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt' #每天自动更新1次
     ]
     for url in urls:
         print(f"处理URL: {url}")
